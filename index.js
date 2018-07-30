df5f739992d61832943a31eae986c81b227754a3
Rina Schwartz <rinadschwartz@protonmail.ch>
2018-07-30 13:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function add2(n) {
  // Feel free to move things around!
 var funkyFunction = function(){ 
  return  function(){
-  return "Funky!"
+  return "FUNKY!"
 }
 }
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
   var theFunk = funkyFunction()();
-  console.log(funkyFunction.toUpperCase()()());
+  console.log(funkyFunction()());
 
 
 
